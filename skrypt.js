00714781d54ff0f4b5559d6c0b8862e9d566931f
Bartosz Kowalczyk <bartosz.kowalczyk@eneris.pl>
2026-04-17 13:06:14 +0200

    Dodanie plików do repozytorium

---
+++ skrypt.js
@@ -0,0 +1,26 @@
+
+async function wyswietlanie() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Response status ${response.status}`);
+            }
+
+        const users = await response.json();
+
+        console.log(users);
+
+        
+        document.getElementById("listah").innerHTML =
+        users.map(user => `<li>${user.name}:    ${user.email}</li>`).join("");
+
+    } 
+    catch (error) {
+        console.error(error.message);
+  }
+}
+
+wyswietlanie();
